7a2c420056a3d6bc3d61803a1d2b665a93bc3fd1
ACEMerlin <gezimerlin@gmail.com>
2019-04-02 02:23:08 +0800

    380

---
+++ 380.insert-delete-getrandom-o1.rb
@@ -0,0 +1,79 @@
+# -*- coding: utf-8 -*-
+#
+# @lc app=leetcode id=380 lang=ruby
+#
+# [380] Insert Delete GetRandom O(1)
+#
+# https://leetcode.com/problems/insert-delete-getrandom-o1/description/
+#
+# Design a data structure that supports all following operations in
+# average O(1) time.
+#
+# insert(val): Inserts an item val to the set if not already present.
+# remove(val): Removes an item val from the set if present.
+# getRandom: Returns a random element from current set of elements. Each
+# element must have the same probability of being returned.
+#
+# Example:
+#
+# // Init an empty set.
+# RandomizedSet randomSet = new RandomizedSet();
+#
+# // Inserts 1 to the set. Returns true as 1 was inserted successfully.
+# randomSet.insert(1);
+#
+# // Returns false as 2 does not exist in the set.
+# randomSet.remove(2);
+#
+# // Inserts 2 to the set, returns true. Set now contains [1,2].
+# randomSet.insert(2);
+#
+# // getRandom should return either 1 or 2 randomly.
+# randomSet.getRandom();
+#
+# // Removes 1 from the set, returns true. Set now contains [2].
+# randomSet.remove(1);
+#
+# // 2 was already in the set, so return false.
+# randomSet.insert(2);
+#
+# // Since 2 is the only number in the set, getRandom always return 2.
+# randomSet.getRandom();
+
+
+
+# 1. hash: { val:index }
+# 2. nums: [val]
+# 3. Always delete the last element from nums, swap with last element if needed.
+
+class RandomizedSet
+
+  def initialize()
+    @st = {}
+    @nums = []
+  end
+
+  def insert(val)
+    return false if @st.has_key?(val)
+    @st[val] = @nums.size
+    @nums << val
+    true
+  end
+
+  def remove(val)
+    return false if !@st.has_key?(val)
+    p = @st[val]
+    if p < @nums.size-1
+      last = @nums[-1]
+      @nums[p] = last
+      @st[last] = p
+    end
+    @st.delete(val)
+    @nums.pop
+    true
+  end
+
+  def get_random()
+    @nums.sample
+  end
+end

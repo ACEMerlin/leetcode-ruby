3f5845d49ff74706a83ec24c5b8d23aba787507c
ACEMerlin <gezimerlin@gmail.com>
2019-04-03 10:40:48 +0800

    47

---
+++ 47.permutations-ii.rb
@@ -0,0 +1,50 @@
+# -*- coding: utf-8 -*-
+#
+# @lc app=leetcode id=47 lang=ruby
+#
+# [47] Permutations II
+#
+# https://leetcode.com/problems/permutations-ii/description/
+#
+# Given a collection of numbers that might contain duplicates, return
+# all possible unique permutations.
+#
+# Example:
+#
+# Input: [1,1,2]
+# Output:
+# [
+# ⁠ [1,1,2],
+# ⁠ [1,2,1],
+# ⁠ [2,1,1]
+# ]
+
+
+# See problem 46. The idea is not to fix the number we've already fixed.
+#
+# See https://en.wikipedia.org/wiki/Permutation#Permutations_of_multisets
+#
+# Example: given "xxy"
+#
+# - x followed by all permutations of x and y (xy and yx)
+# - do not fix the second x in position 0
+# - y followed by all permutations of x and x (xx)
+
+# @param {Integer[]} nums
+# @return {Integer[][]}
+def permute_unique(nums, k=0, ans=[])
+  ans << nums.dup and return if k == nums.size
+  seen = Set.new
+  (k...nums.size).each do |i|
+    next if seen.include?(nums[i])
+    seen << nums[i]
+    swap(nums, k, i)
+    permute_unique(nums, k+1, ans)
+    swap(nums, k, i)
+  end
+  ans
+end
+
+def swap(nums, i, j)
+  nums[i], nums[j] = nums[j], nums[i]
+end

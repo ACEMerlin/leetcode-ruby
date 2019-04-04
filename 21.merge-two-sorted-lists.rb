368ce3464af4204be0b9e9768aca9fd856eeb4e0
ACEMerlin <gezimerlin@gmail.com>
2019-04-04 19:02:56 +0800

    21

---
+++ 21.merge-two-sorted-lists.rb
@@ -0,0 +1,40 @@
+# -*- coding: utf-8 -*-
+#
+# @lc app=leetcode id=21 lang=ruby
+#
+# [21] Merge Two Sorted Lists
+#
+# https://leetcode.com/problems/merge-two-sorted-lists/description/
+#
+# Merge two sorted linked lists and return it as a new list. The new list
+# should be made by splicing together the nodes of the first two lists.
+#
+# Example:
+#
+# Input: 1->2->4, 1->3->4
+# Output: 1->1->2->3->4->4
+#
+# Definition for singly-linked list.
+# class ListNode
+#     attr_accessor :val, :next
+#     def initialize(val)
+#         @val = val
+#         @next = nil
+#     end
+# end
+
+
+# @param {ListNode} l1
+# @param {ListNode} l2
+# @return {ListNode}
+def merge_two_lists(l1, l2)
+  return l2 if l1.nil?
+  return l1 if l2.nil?
+  if l1.val <= l2.val
+    l1.next = merge_two_lists(l1.next, l2)
+    l1
+  else
+    l2.next = merge_two_lists(l1, l2.next)
+    l2
+  end
+end
